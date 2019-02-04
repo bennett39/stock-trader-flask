4fced32f2a99b5d05e91959e4e6022825dcbc33f
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2019-02-04 12:00:20 -0500

    Remove unneeded dependencies from application.py

--- application.py
+++ application.py
@@ -1,7 +1,5 @@
-from datetime import datetime
 from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
-from flask_heroku import Heroku
 from flask_sqlalchemy import SQLAlchemy
 
 from config import app, db
