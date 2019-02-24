d50f035ca1484f8627ad1e55e7a052f4ebc9c36c
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2019-02-24 18:44:57 -0500

    Start writing test suite & update htmlcov

--- test_basic.py
+++ test_basic.py
@@ -2,7 +2,7 @@ import urllib
 from flask_testing import TestCase
 
 from config import create_app, db
-from queries import User, Transaction, Stock
+import queries as q
 
 class MyTest(TestCase):
     SQLALCHEMY_DATABASE_URI = "sqlite://"
@@ -22,16 +22,57 @@ class MyTest(TestCase):
         db.drop_all()
 
     def test_db_connection(self):
-        user = User(username="test", password_hash="test")
+        """Create an instance of all models and add to db"""
+        user = q.User(
+                username="test", 
+                password_hash="test"
+                )
+        transaction = q.Transaction(
+                user_id=1,
+                stock_id=1,
+                quantity=1,
+                price=1
+                )
+        stock = q.Stock(
+                symbol='a',
+                name='a'
+                )
         db.session.add(user)
+        db.session.add(transaction)
+        db.session.add(stock)
         db.session.commit()
         assert user in db.session
 
-    def test_user_count(self):
-        a = User(username="aaa", password_hash="aaa")
-        b = User(username="bbb", password_hash="bbb")
+    def test_delete_transaction(self):
+        """Deletes all transactions for given user"""
+        a = q.Transaction(user_id=1, stock_id=1, quantity=1, price=1)
+        b = q.Transaction(user_id=1, stock_id=2, quantity=2, price=2)
         db.session.add(a)
         db.session.add(b)
         db.session.commit()
-        users = User.query.all()
-        assert sum(1 for u in users) == 2
+        q.delete_transactions_by_user(1)
+        assert sum(1 for t in q.Transaction.query.all()) == 0
+
+    def test_insert_stock(self):
+        """Creates a new stock in database"""
+        q.insert_stock('a', 'a')
+        assert sum(1 for s in q.Stock.query.all()) == 1
+
+    def test_insert_transaction(self):
+        """Creates a new transaction in database"""
+        q.insert_transaction(1, 1, 1, 1)
+        assert sum(1 for s in q.Transaction.query.all()) == 1
+
+    def test_insert_user(self):
+        """Creates a new user in database"""
+        q.insert_user('a', 'a')
+        assert sum(1 for u in q.User.query.all()) == 1
+
+    def test_select_user_id(self):
+        a = q.User(username='a', password_hash='a')
+        db.session.add(a)
+        db.session.commit()
+        a = q.User.query.first()
+        assert a == q.select_user_by_id(a.id)
+
+    
